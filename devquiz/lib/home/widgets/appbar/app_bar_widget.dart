62eb7afd9eb1f2ed9f03b8c29a13960d19ba0065
TiagoCastilhos <tiagoxd13@hotmail.com>
2021-04-21 12:00:35 -0300

    primeira aula

---
+++ devquiz/lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,56 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: "Tiago",
+                                style: AppTextStyles.titleBold,
+                              ),
+                            ]),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/a411fc38106877.5756241ce657f.jpg"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0, 1),
+                  child: ScoreCardWidget(),
+                )
+              ],
+            ),
+          ),
+        );
+}
